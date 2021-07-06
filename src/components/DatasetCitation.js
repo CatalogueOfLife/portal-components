e04cbd8ee3bbe81e5906c79f1d38dcb164f8abc0
Thomas Stjernegaard <tsjeppesen@gbif.org>
2021-07-06 09:03:15 +0200

    closes https://github.com/CatalogueOfLife/portal-components/issues/19

--- src/components/DatasetCitation.js
+++ src/components/DatasetCitation.js
@@ -5,7 +5,7 @@ const Citation = ({ dataset }) => {
 
   return (
     <div className="col-dataset-citation">
-      <div className="col-dataset-citation-title">{dataset.citation || dataset.title}</div>
+      <div className="col-dataset-citation-title">{dataset.citation ? <span dangerouslySetInnerHTML={{__html: dataset.citation}}></span> : dataset.title}</div>
       <div className="col-dataset-citation-source"> - accessed through <a href={`https://data.catalogueoflife.org/dataset/${dataset.key}`}>Catalogue of Life Checklistbank</a></div>
     </div>
   );
